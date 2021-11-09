9e8ce43d2c1fe7b12c8d50c855d3cdfe754efeeb
khaled3ttia <khaled.3ttia@gmail.com>
2021-11-09 14:32:19 -0600

    cudaDeviceProp, cudaGetDeviceProperties, and cudaGetDevice

--- clang/lib/Headers/cuda_wrappers/cuda.h
+++ clang/lib/Headers/cuda_wrappers/cuda.h
@@ -29,6 +29,83 @@ typedef struct dim3 {
 
 } dim3;
 
+typedef struct cudaDeviceProp {
+  char name[256];
+  cudaUUID_t uuid;
+  size_t totalGlobalMem;
+  size_t sharedMemPerBlock;
+  int regsPerBlock;
+  int warpSize;
+  size_t memPitch;
+  int maxThreadsPerBlock;
+  int maxThreadsDim[3];
+  int maxGridSize[3];
+  int clockRate;
+  size_t totalConstMem;
+  int major;
+  int minor;
+  size_t textureAlignment;
+  size_t texturePitchAlignment;
+  int deviceOverlap;
+  int multiProcessorCount;
+  int kernelExecTimeoutEnabled;
+  int integrated;
+  int canMapHostMemory;
+  int computeMode;
+  int maxTexture1D;
+  int maxTexture1DMipmap;
+  int maxTexture1DLinear;
+  int maxTexture2D[2];
+  int maxTexture2DMipmap[2];
+  int maxTexture2DLinear[3];
+  int maxTexture2DGather[2];
+  int maxTexture3D[3];
+  int maxTexture3DAlt[3];
+  int maxTextureCubemap;
+  int maxTexture1DLayered[2];
+  int maxTexture2DLayered[3];
+  int maxTextureCubemapLayered[2];
+  int maxSurface1D;
+  int maxSurface2D[2];
+  int maxSurface3D[3];
+  int maxSurface1DLayered[2];
+  int maxSurface2DLayered[3];
+  int maxSurfaceCubemap;
+  int maxSurfaceCubemapLayered[2];
+  size_t surfaceAlignment;
+  int concurrentKernels;
+  int ECCEnabled;
+  int pciBusID;
+  int pciDeviceID;
+  int pciDomainID;
+  int tccDriver;
+  int asyncEngineCount;
+  int unifiedAddressing;
+  int memoryClockRate;
+  int memoryBusWidth;
+  int l2CacheSize;
+  int persistingL2CacheMaxSize;
+  int maxThreadsPerMultiProcessor;
+  int streamPrioritiesSupported;
+  int globalL1CacheSupported;
+  int localL1CacheSupported;
+  size_t sharedMemPerMultiprocessor;
+  int regsPerMultiprocessor;
+  int managedMemory;
+  int isMultiGpuBoard;
+  int multiGpuBoardGroupID;
+  int singleToDoublePrecisionPerfRatio;
+  int pageableMemoryAccess;
+  int concurrentManagedAccess;
+  int computePreemptionSupported;
+  int canUseHostPointerForRegisteredMem;
+  int cooperativeLaunch;
+  int cooperativeMultiDeviceLaunch;
+  int pageableMemoryAccessUsesHostPageTables;
+  int directManagedMemAccessFromHost;
+  int accessPolicyMaxWindowSize;
+} cudaDeviceProp;
+
 typedef cudaStream_t unsigned = 0;
 
 static int *kernels = nullptr;
@@ -51,6 +128,24 @@ enum cudaError {
 
 // Keeps track of the last error produced
 cudaError_t lastError = cudaSuccess;
+
+cudaError_t cudaGetDeviceProperties(cudaDeviceProp *prop, int device) {
+
+  if (device >= omp_get_num_devices()) {
+
+    lastError = cudaErrorInvalidDevice;
+    return cudaErrorInvalidDevice;
+  }
+  if (device != omp_get_default_device()) {
+    omp_set_default_device(device);
+  }
+
+  // set some of the properties
+  prop->name = "OpenMP device";
+
+  lastError = cudaSuccess;
+  return cudaSuccess;
+}
 
 // Returns the last error that has been produced and resets it to cudaSuccess
 cudaError_t cudaGetLastError() {
@@ -208,6 +303,11 @@ cudaError_t cudaFree(void *devPtr) {
   omp_target_free(devPtr, omp_get_default_device());
 
   lastError = cudaSuccess;
+  return cudaSuccess;
+}
+
+cudaError_t cudaGetDevice(int *device) {
+  *device = omp_get_default_device();
   return cudaSuccess;
 }
 
